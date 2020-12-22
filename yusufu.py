d671905390ef34cd79ec79d05ed8c5bfe619677c
mrrv4 <76444056+mrrv4@users.noreply.github.com>
2020-12-22 13:43:10 +0300

    Add files via upload

---
+++ yusufu.py
@@ -0,0 +1,18 @@
+n = 5
+m = 3.2
+a = "Ayko"
+b = 6
+c = "A"
+ 
+
+print(f'Values: {n}')
+print(f'Values: {m}')
+print("Values: {}".format(a))
+print("Values: {}".format(b))
+print("Values: {}".format(c))
+
+print(f'Types: {type(n)}')
+print(f'Types: {type(n)}')
+print("Types: {}".format(type(a)))
+print("Types: {}".format(type(b)))
+print("Types: {}".format(type(c)))
