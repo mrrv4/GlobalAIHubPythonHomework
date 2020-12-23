d828677bc1d99defb5d3125147f50ff7fd527ee8
mrrv4 <76444056+mrrv4@users.noreply.github.com>
2020-12-23 19:18:42 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,11 @@
+a = int(input("İnteger Value: "))
+b = str(input("String Value: "))
+c = float(input("Float Value: "))
+d = bool(input("Boolean Value: "))
+e = complex(input("Complex Value:"))
+
+print("Class :{}".format(type(a)))
+print("Class :{}".format(type(b)))
+print("Class :{}".format(type(c)))
+print("Class :{}".format(type(d)))
+print("Class :{}".format(type(e)))
